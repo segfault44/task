91e3007118f44413229c31bfeeddb16e32ff5310
segfault44 <segfault44@gmail.com>
2026-01-21 11:22:47 +0300

    Add category id to dashboard table entries

--- views/dashboard/dashboard_table.js
+++ views/dashboard/dashboard_table.js
@@ -10,12 +10,25 @@ const DashboardTable = {
         { id: "title", header: ["Film Title", { content: "textFilter" }], sort: "text", fillspace: true },
         { id: "year", header: ["Released", { content: "textFilter" }], sort: "text", width: 80 },
         { id: "votes", header: ["Votes", { content: "textFilter" }], template: `#votes# <span class="remove_btn webix_icon wxi-trash"></span>`, sort: "text", width: 100 },
+        { id: "category_id", header: ["Category", { content: "selectFilter" }], sort: "text", collection: "/data/categories.json" },
     ],
     onClick: {
         /** @this {webix.ui.datatable} */
         remove_btn(_ev, id) {
             this.remove(id);
             return false;
+        }
+    },
+    scheme: {
+        /**
+         * @this {webix.ui.datatable}
+         * @param {object} obj
+         */
+        $init(obj) {
+            // Generate a random integer in range [1, 4]
+            const cat = Math.floor(Math.random() * 4.0) + 1.0;
+            // Assign it as a category id
+            obj["category_id"] = cat;
         }
     },
     url: "/data/data.json",
